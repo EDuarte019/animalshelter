58044a4cee345adbbb12b639e6caeada2493d6be
Jacob Gordon <jgordon024@west-mec.org>
2019-09-05 15:08:44 -0700

    Carousel + HTML +CSS
    Added footers to each page, worked on contact.css some but the problem is still occurring. Worked on the index file as well and added some small media query styles for the images in the about section. Also created and worked on a JS file using ES6 for the carousel, but it's not working properly.

---
+++ Jacob/carousel.js
@@ -0,0 +1,20 @@
+const innerCarousel = document.getElementsByClassName("innerCarousel");
+const next = document.getElementsByClassName("next");
+const prev = document.getElementsByClassName("prev");
+
+const images = [document.getElementsByClassName("item-1"),document.getElementsByClassName("item-2"),document.getElementsByClassName("item-3")];
+
+let i= images.length;
+
+// Adding the function to go to the next slide
+
+next.onclick = () =>{
+    i = (i<images.length)?(i=i+1):(i=1);
+    innerCarousel = images[i-1];
+} 
+
+// Adding the function to go to the previous slide
+next.onclick = () =>{
+    i = (i<images.length+1 && i>1)?(i=i-1):(i=images.length);
+    innerCarousel = images[i-1];
+} 
